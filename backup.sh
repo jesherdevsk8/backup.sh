32345afba5a3586dc159a4ce339a22799261cbf7
Jesher Minelli Alves <jesheralves@claretiano.edu.br>
2023-02-18 18:05:08 -0300

    feat: add monitoring backup when its running

--- backup.sh
+++ backup.sh
@@ -1,52 +1,44 @@
 #!/usr/bin/env bash
-#
-# backup.sh - Faz backup completo para um dispositivo móvel
-# Autor: Jesher Minelli Alves <jesherdevsk8@gmail.com>
-# licença: GNU GPLv2
-# Histórico: 
-# 	   Versão inicial v1.0 01/03/2022 [ Jesher ]
-#      Próxima feature [ Torne-o em um backup incremental ]
-#	   Pŕoxima feature [ Fazer uma barra de progresso ]
-#
-# Testado: bash versão 5.0.17
-#
-# Torne o script executável em qualquer lugar
-# Ex: sudo ln -s $HOME/github/Backup/backup.sh /usr/local/bin/backup
-# ADICIONE AO CRON CASO PREFIRA
-# execute como root - $ crontab -e
-# 0 9 * * * /usr/local/sbin/backup.sh  <- Todo dia as 09:00 horas
-# Visite o site: https://crontab.guru/
-#
-#
-# -------------- VARIÁVEIS
+# ---------------------- [ Script backup ] -----------------------+
+#        Script   : backup.sh                                     |
+#        Author   : Jesher Minelli <jesherdevsk8@gmail.com>       |
+#        Date     : ter 21 jun 2022                               |  
+#        Use      : ln -s $PWD/backup.sh /usr/local/bin/bkp       |
+# ----------------------------------------------------------------+
 
-backup_path="/home/jesher/Documentos" # Diretório para backup
-external_storage="/media/jesher/kingston" # Dispositivo externo de destino
-destination="/media/jesher/kingston/backups" # Diretório para ser criado dentro do dispositivo externo
-date_log="$(date)" # Data erro log
-date_format=$(date "+%A %d-%m-%Y") # Data do backup
-final_archive="backup-$date_format.tar.gz" # Formato do arquivo
-log_file="/var/log/daily-backup.log" # Arquivo de log
-# Monitorar o arquvo de log
-# tail -f /var/log/daily-backup.log ou less +F /var/log/daily-backup.log
+backup_path="$HOME/Documentos/"
+external_storage="/media/jesher/Ventoy"
+destination="/media/jesher/Ventoy/backups"
+date_log=$(date "+%a %d %b %Y %H:%M:%S")
+date_format=$(date "+%A %d-%m-%Y")
+final_archive="backup-$date_format.tar.gz"
+log_file="$PWD/backup.log"
 
-# -------------- TESTES
+[[ $UID -eq 0 ]] && echo "Do not need super user...." && exit 1
 
-[[ "$UID" -ne "0" ]] && { echo "Precisa de Root!!!"; exit 1 ;}
+monit_backup(){
+  if [ -f "$PWD/monit.sh" ]; then
+    . "$PWD/monit.sh" &
+    monit_pid=$!
+  fi
+}
 
-if ! mountpoint -q -- $external_storage; then # Pendrive plugado na máquina?
-	printf "[$date_log] DEVICE NOT MOUNTED in: $external_storage CHECK IT.\n" >> $log_file
-	exit 1
-else 
-	[[ ! -d "$destination" ]] && mkdir -p "$destination"
-fi
+backup(){
+  monit_backup
+  if tar -cpSzf "$destination/$final_archive" "$backup_path" &>/dev/null; then
+    printf "[ $date_log ] BACKUP SUCCESSFULLY COMPLETED.\n" >> "$log_file"
+    find "${destination}/" -name '*.gz' -mtime +1 -delete # Deleting files greater than two days
+    kill $monit_pid
+  else
+    printf "[ $date_log ] THERE WAS AN ERROR BACKING UP...!!\n" >> "$log_file"
+    kill $monit_pid
+  fi
+}
 
-# --------------- EXECUÇÃO
-
-if tar -cpSzf "${external_storage}/${final_archive}" "$backup_path" &>/dev/null; then
-	printf "[$date_log] BACKUP SUCCESS.\n" >> $log_file
+if ! mountpoint -q -- "$external_storage"; then # Device mounted ?
+  printf "[ $date_log ] No such file or directory in "$destination"\n" >> "$log_file"
+  exit 1
 else
-	printf "[$date_log] BACKUP ERROR...!!\n" >> $log_file
+  mkdir -p "$destination"
+	backup
 fi
-
-find ${destination}/*.gz -mtime +5 -delete # Excluir arquivos com mais de cinco dias
---
+++ monit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# ---------------------- [ Script backup ] ---------------------------+
+#        Script      : monit.sh                                       |
+#		     Description : monitoring backup                              | 
+#        Author      : Jesher Minelli <jesherdevsk8@gmail.com>        |
+#        Date        : ter 21 jun 2022                                |  
+#        Use         : ./monit.sh                                     |
+# --------------------------------------------------------------------+
+external_storage="/media/jesher/Ventoy"
+backup_dir="${external_storage}/backups"
+
+if mountpoint -q -- "$external_storage"; then # Device mounted ?
+  if [[ -d "${backup_dir}" ]]; then
+    while true; do
+        sleep 2; clear
+        du -sh "${backup_dir}"/*.tar.gz
+    done
+  else
+    echo "No such file or directory"; exit 1
+  fi
+fi
